6b13afebe86d22524da6464107bea9c6c4dac3f7
Olebraende <64941179+Olebraende@users.noreply.github.com>
2025-05-14 12:45:20 +0200

    refactor: streamline database connection setup with Sequelize and MariaDB

--- sequelize.js
+++ sequelize.js
@@ -3,32 +3,34 @@ dotenv.config();
 
 import { Sequelize } from '@sequelize/core';
 import { MariaDbDialect } from '@sequelize/mariadb';
-// const mariadb = require('mariadb');
-// const pool = mariadb.createPool({
-//      host: 'mydb.com', 
-//      user:'myUser', 
-//      password: 'myPassword',
-//      connectionLimit: 5
-// });
+import mariadb from 'mariadb';
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
+  allowPublicKeyRetrieval: true, // Allow RSA key retrieval
+  cachingRsaPublicKey: true,     // Cache the public key
+  port: 3306,
+});
 
 const db = new Sequelize({
-dialect: MariaDbDialect,
-  database: process.env.DB_NAME,
+  dialect: MariaDbDialect,
   user: process.env.DB_USER,
-  password:  process.env.DB_PASSWORD,
-  host:  process.env.DB_HOST,
-  port: 3306,
-  showWarnings: true,
-  connectTimeout: 1000,  
-}
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
-    // {
-    //     host: process.env.DB_HOST,
-    //     port: process.env.DB_PORT,
-    //     dialect: MariaDbDialect
-    // }
-);
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  pool: pool,  // Use the custom pool
+  connectTimeout: 1000,
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 export default db;
